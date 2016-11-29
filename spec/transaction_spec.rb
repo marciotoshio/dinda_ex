b212c2690f2786d046f3623e1e0a4a8cb6322b16
Marcio Toshio Ide <contato@marciotoshio.com.br>
2016-11-29 15:59:46 -0200

    Update transaction_spec.rb

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 
 describe DindaEx::Transaction do
-  context 'with an transaction which is credit' do
+  context 'with a transaction which is credit' do
     let(:transaction) { DindaEx::Transaction.new(1, 1000) }
     it {expect(transaction.is_debit?).to be false }
   end
   
-  context 'with an transaction which is debit' do
+  context 'with a transaction which is debit' do
     let(:transaction) { DindaEx::Transaction.new(1, -1000) }
     it {expect(transaction.is_debit?).to be true  }
   end
